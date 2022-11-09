24bbb9e49da051231dc8aea6dc25c6bb1daa5564
dumbleedore <lucas.pari@icloud.com>
2022-11-09 14:16:34 -0300

    starting tests

---
+++ server/src/test/java/com/lucas/taskmanager/service/TarefaServiceTest.java
@@ -0,0 +1,28 @@
+package com.lucas.taskmanager.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TarefaServiceTest {
+
+    @Test
+    void getTarefas() {
+    }
+
+    @Test
+    void createTarefa() {
+    }
+
+    @Test
+    void deleteTarefa() {
+    }
+
+    @Test
+    void getAllTarefas() {
+    }
+
+    @Test
+    void updateTarefa() {
+    }
+}
